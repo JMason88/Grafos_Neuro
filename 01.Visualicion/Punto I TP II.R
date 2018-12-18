d6e4ca927dc147fc2b85b76078eeb35f697fe484
Mario <helgueta@hotmail.com>
2018-12-18 16:17:48 -0300

    Update Punto1

--- 01.Visualicion/Punto I TP II.R
+++ 01.Visualicion/Punto I TP II.R
@@ -296,8 +296,46 @@ summary(reg)$r.square
 
 cozf=coef(reg)
 
+plot(probability ~degree)
+
+plot(probability ~ degree, log = "xy")
+
+power.law.fit = function(x)exp(cozf[[1]]+cozf[[2]]*log(x))
+
+curve(power.law.fit, col = "red", add =T)
+
+g.big.ba = barabasi.game(2000)
+g.big.er = erdos.renyi.game(2000, 0.1)
+
+plot_degree_distribution(g.big.ba)
 
 
+# plot and fit the power law distribution
+fit_power_law = function(graph) {
+  # calculate degree
+  d = degree(graph, mode = "all")
+  dd = degree.distribution(graph, mode = "all", cumulative = FALSE)
+  degree = 1:max(d)
+  probability = dd[-1]
+  # delete blank values
+  nonzero.position = which(probability != 0)
+  probability = probability[nonzero.position]
+  degree = degree[nonzero.position]
+  reg = lm(log(probability) ~ log(degree))
+  cozf = coef(reg)
+  power.law.fit = function(x) exp(cozf[[1]] + cozf[[2]] * log(x))
+  alpha = -cozf[[2]]
+  R.square = summary(reg)$r.squared
+  print(paste("Alpha =", round(alpha, 3)))
+  print(paste("R square =", round(R.square, 3)))
+  # plot
+  plot(probability ~ degree, log = "xy", xlab = "Degree (log)", ylab = "Probability (log)", 
+       col = 1, main = "Degree Distribution")
+  curve(power.law.fit, col = "red", add = T, n = length(d))
+}
+
+
+fit_power_law(g.big.ba)
 
 ################### CAMINO MINIMO ##################
 
