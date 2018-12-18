0a5052b9361c7491698c3fb9a91a2b004f517d73
JMason88 <30500349+JMason88@users.noreply.github.com>
2018-12-18 02:10:31 -0300

    Punto 2 + Opcional 1 Completo

--- 02.Comunidades_Modularidad/comunidades_modularidad.R
+++ 02.Comunidades_Modularidad/comunidades_modularidad.R
@@ -7,8 +7,8 @@ source('jk.modularity.R')
 libraryRequireInstall('igraph')
 libraryRequireInstall('visNetwork')
 libraryRequireInstall('ggplot2')
-
-
+libraryRequireInstall('RColorBrewer')
+libraryRequireInstall(reshape2)
 
 ####################### Data Loading #######################
 
@@ -20,35 +20,187 @@ W <- data_reader(x = "W_")
 ############################################################
 
 
-######################## Modularity ########################
+############# Louvain - Nro Comunidades ####################
 
 N = dim(N1)[1]
 Nmaxlinks = N*(N-1)
+
 nlist = seq(100,2000,100)
 dlist = array(data=NA, dim=length(nlist))
 W.mlist = array(data=NA, dim=length(nlist))
 N1.mlist = array(data=NA, dim=length(nlist))
 N2.mlist = array(data=NA, dim=length(nlist))
 N3.mlist = array(data=NA, dim=length(nlist))
+Random.mlist = array(data=NA, dim = length(nlist))
+
 k = 0
 for (n in nlist) {
   k = k+1
   dlist[k] = n/Nmaxlinks
-  W.mlist[k] = jk.modularity(W,n,"Louvain")
-  N1.mlist[k] = jk.modularity(N1,n,"Louvain")
-  N2.mlist[k] = jk.modularity(N2,n,"Louvain")
-  N3.mlist[k] = jk.modularity(N3,n,"Louvain")
+  W.mlist[k] = jk.modularity(W,n,"Louvain")[2]
+  N1.mlist[k] = jk.modularity(N1,n,"Louvain")[2]
+  N2.mlist[k] = jk.modularity(N2,n,"Louvain")[2]
+  N3.mlist[k] = jk.modularity(N3,n,"Louvain")[2]
+  
+  ##  Grafo Random
+  random_graph <- sample_gnm(116,n)
+  random_louvain <- cluster_louvain(random_graph)
+  Random.mlist[k] <- length(random_louvain)
 }
 
-plot.communities(jk.modularity(W,n,"Louvain"))
 
-df <- data.frame(dlist,W.mlist,N1.mlist,N2.mlist,N3.mlist)
+df_comu <- data.frame(dlist,W.mlist,N1.mlist,N2.mlist,N3.mlist,Random.mlist)
+
+color <- brewer.pal(5, name = 'Dark2')
+
+##Then rearrange your data frame
+
+dd_comu = melt(df_comu, id=c("dlist"))
+
+ggplot(dd_comu, aes(x = dlist, y = value, group=variable, color=variable)) +                    # basic graphical object
+  geom_line(size=1.2) +  # first layer
+  scale_color_brewer(palette = "Dark2") +
+  scale_linetype_manual(c("solid","solid","solid","solid","dashed")) +
+  labs(title="Louvain - Cantidad de Comunidades",x="Densidad de Aristas(d)", y = "Cantidad de Comunidades (Nc)") +
+  theme_classic() +
+  theme(legend.position="bottom")
 
 
 
-ggplot(df, aes(dlist)) +                    # basic graphical object
-  geom_line(aes(y=W.mlist), colour="black") +  # first layer
-  geom_line(aes(y=N1.mlist), colour="green")  + # second layer
-  geom_line(aes(y=N2.mlist), colour="blue")  + # second layer
-  geom_line(aes(y=N3.mlist), colour="red") +  # second layer
-  theme_minimal()
+
+################## Louvain Modularity ########################
+
+N = dim(N1)[1]
+Nmaxlinks = N*(N-1)
+
+nlist = seq(100,2000,100)
+dlist = array(data=NA, dim=length(nlist))
+W.mlist = array(data=NA, dim=length(nlist))
+N1.mlist = array(data=NA, dim=length(nlist))
+N2.mlist = array(data=NA, dim=length(nlist))
+N3.mlist = array(data=NA, dim=length(nlist))
+Random.mlist = array(data=NA, dim = length(nlist))
+
+k = 0
+for (n in nlist) {
+  k = k+1
+  dlist[k] = n/Nmaxlinks
+  W.mlist[k] = jk.modularity(W,n,"Louvain")[1]
+  N1.mlist[k] = jk.modularity(N1,n,"Louvain")[1]
+  N2.mlist[k] = jk.modularity(N2,n,"Louvain")[1]
+  N3.mlist[k] = jk.modularity(N3,n,"Louvain")[1]
+  
+  ##  Grafo Random
+  random_graph <- sample_gnm(116,n)
+  random_louvain <- cluster_louvain(random_graph)
+  Random.mlist[k] <- modularity(random_graph, random_louvain$membership)
+}
+
+
+df <- data.frame(dlist,W.mlist,N1.mlist,N2.mlist,N3.mlist,Random.mlist)
+
+color <- brewer.pal(5, name = 'Dark2')
+
+##Then rearrange your data frame
+dd = melt(df, id=c("dlist"))
+
+ggplot(dd, aes(x = dlist, y = value, group=variable, color=variable)) +                    # basic graphical object
+  geom_line(size=1.2) +  # first layer
+  scale_color_brewer(palette = "Dark2") +
+  scale_linetype_manual(c("solid","solid","solid","solid","dashed")) +
+  labs(title="Louvain - Coeficiente de Modularidad",x="Densidad de Aristas(d)", y = "Coeficiente de Modularidad (Q)") +
+  theme_classic() +
+  theme(legend.position="bottom")
+
+
+############# Girvan-Newman Nro Comunidades ####################
+
+N = dim(N1)[1]
+Nmaxlinks = N*(N-1)
+
+nlist = seq(100,2000,100)
+dlist = array(data=NA, dim=length(nlist))
+W.mlist = array(data=NA, dim=length(nlist))
+N1.mlist = array(data=NA, dim=length(nlist))
+N2.mlist = array(data=NA, dim=length(nlist))
+N3.mlist = array(data=NA, dim=length(nlist))
+Random.mlist = array(data=NA, dim = length(nlist))
+
+k = 0
+for (n in nlist) {
+  k = k+1
+  dlist[k] = n/Nmaxlinks
+  W.mlist[k] = jk.modularity(W,n,"Girvan-Newman")[2]
+  N1.mlist[k] = jk.modularity(N1,n,"Girvan-Newman")[2]
+  N2.mlist[k] = jk.modularity(N2,n,"Girvan-Newman")[2]
+  N3.mlist[k] = jk.modularity(N3,n,"Girvan-Newman")[2]
+  
+  ##  Grafo Random
+  random_graph <- sample_gnm(116,n)
+  random_louvain <- cluster_edge_betweenness(random_graph)
+  Random.mlist[k] <- length(random_louvain)
+}
+
+
+df_comu <- data.frame(dlist,W.mlist,N1.mlist,N2.mlist,N3.mlist,Random.mlist)
+
+color <- brewer.pal(5, name = 'Dark2')
+
+##Then rearrange your data frame
+
+dd_comu = melt(df_comu, id=c("dlist"))
+
+ggplot(dd_comu, aes(x = dlist, y = value, group=variable, color=variable)) +                    # basic graphical object
+  geom_line(size=1.2) +  # first layer
+  scale_color_brewer(palette = "Dark2") +
+  scale_linetype_manual(c("solid","solid","solid","solid","dashed")) +
+  labs(title="Girvan-Newman - Cantidad de Comunidades",x="Densidad de Aristas(d)", y = "Cantidad de Comunidades (Nc)") +
+  theme_classic() +
+  theme(legend.position="bottom")
+
+
+
+
+################## Girvan-Newman Modularity ########################
+
+N = dim(N1)[1]
+Nmaxlinks = N*(N-1)
+
+nlist = seq(100,2000,100)
+dlist = array(data=NA, dim=length(nlist))
+W.mlist = array(data=NA, dim=length(nlist))
+N1.mlist = array(data=NA, dim=length(nlist))
+N2.mlist = array(data=NA, dim=length(nlist))
+N3.mlist = array(data=NA, dim=length(nlist))
+Random.mlist = array(data=NA, dim = length(nlist))
+
+k = 0
+for (n in nlist) {
+  k = k+1
+  dlist[k] = n/Nmaxlinks
+  W.mlist[k] = jk.modularity(W,n,"Girvan-Newman")[1]
+  N1.mlist[k] = jk.modularity(N1,n,"Girvan-Newman")[1]
+  N2.mlist[k] = jk.modularity(N2,n,"Girvan-Newman")[1]
+  N3.mlist[k] = jk.modularity(N3,n,"Girvan-Newman")[1]
+  
+  ##  Grafo Random
+  random_graph <- sample_gnm(116,n)
+  random_louvain <- cluster_edge_betweenness(random_graph)
+  Random.mlist[k] <- modularity(random_graph, random_louvain$membership)
+}
+
+
+df <- data.frame(dlist,W.mlist,N1.mlist,N2.mlist,N3.mlist,Random.mlist)
+
+color <- brewer.pal(5, name = 'Dark2')
+
+##Then rearrange your data frame
+dd = melt(df, id=c("dlist"))
+
+ggplot(dd, aes(x = dlist, y = value, group=variable, color=variable)) +                    # basic graphical object
+  geom_line(size=1.2) +  # first layer
+  scale_color_brewer(palette = "Dark2") +
+  scale_linetype_manual(c("solid","solid","solid","solid","dashed")) +
+  labs(title="Girvan-Newman - Coeficiente de Modularidad",x="Densidad de Aristas(d)", y = "Coeficiente de Modularidad (Q)") +
+  theme_classic() +
+  theme(legend.position="bottom")
